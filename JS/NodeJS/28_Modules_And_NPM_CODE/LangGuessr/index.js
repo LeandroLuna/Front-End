06d41705ed73a105bab58573744002a18b88cee1
Leandro Luna <leandro.j.luna@gmail.com>
2022-06-19 19:12:01 -0300

    new express files

--- JS/NodeJS/28_Modules_And_NPM_CODE/LangGuessr/index.js
+++ JS/NodeJS/28_Modules_And_NPM_CODE/LangGuessr/index.js
@@ -6,7 +6,7 @@ const colors = require("colors")
 const input = process.argv[2];
 const langCode = franc(input);
 if (langCode === 'und') {
-    console.log("SORRY, COULDN'T FIGURE IT OUT! TRY WITH MORE SAMPLE TEXT!".red)
+    console.log("SORRY, COULDN'T FIGURE IT OUT! TRY WITH MORE SAMPLE TEXT!".blue)
 } else {
     const language = langs.where("3", langCode);
     console.log(`Our best guess is: ${language.name}`.green)
