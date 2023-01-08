aef5fde982dc62a9f0f9b4201ce9443d14ec405b
Leandro Luna <leandro.j.luna@gmail.com>
2023-01-08 19:47:54 -0300

    adding services

--- Angular/RecipesProject/src/app/recipes/recipe-list/recipe-list.component.ts
+++ Angular/RecipesProject/src/app/recipes/recipe-list/recipe-list.component.ts
@@ -1,4 +1,5 @@
-import { EventEmitter } from '@angular/core';
+import { RecipeService } from './../recipe.service';
+import { EventEmitter, OnInit } from '@angular/core';
 import { Recipe } from './../recipe.model';
 import { Component, Output } from '@angular/core';
 
@@ -7,14 +8,12 @@ import { Component, Output } from '@angular/core';
   templateUrl: './recipe-list.component.html',
   styleUrls: ['./recipe-list.component.css']
 })
-export class RecipeListComponent {
-  @Output() recipeWasSelected = new EventEmitter<Recipe>();
-  recipes: Recipe[] = [
-    new Recipe('A test recipe', 'This is a simply test', 'https://www.foodandwine.com/thmb/gE_M3yiTJCgZPKSOEVmjQowKv9E=/750x0/filters:no_upscale():max_bytes(150000):strip_icc()/Tamarind-Chicken-FT-RECIPE0522-80072d93f7bc4bc7abf1dcf5b5317b0c.jpg'),
-    new Recipe('A test recipe - 2', 'This is a simply test - 2', 'https://www.foodandwine.com/thmb/gE_M3yiTJCgZPKSOEVmjQowKv9E=/750x0/filters:no_upscale():max_bytes(150000):strip_icc()/Tamarind-Chicken-FT-RECIPE0522-80072d93f7bc4bc7abf1dcf5b5317b0c.jpg')
-  ]
+export class RecipeListComponent implements OnInit {
+  recipes: Recipe[];
 
-  onRecipeSelected(recipe: Recipe){
-    this.recipeWasSelected.emit(recipe);
+  constructor(private recipeService: RecipeService){}
+
+  ngOnInit(){
+    this.recipes = this.recipeService.getRecipeslist();
   }
 }
