754a79db8901b2108c4cede7e0024f0b95547ad9
Leandro Luna <leandro.j.luna@gmail.com>
2023-01-09 11:21:01 -0300

    query params and fragments, nested routes

--- Angular/routing/src/app/app.module.ts
+++ Angular/routing/src/app/app.module.ts
@@ -20,15 +20,27 @@ const appRoutes: Routes = [
   },
   {
     path: 'users',
-    component: UsersComponent
-  },
-  {
-    path: 'users/:id/:name',
-    component: UserComponent
+    component: UsersComponent,
+    children: [
+      {
+        path: ':id/:name',
+        component: UserComponent
+      },
+    ]
   },
   {
     path: 'servers',
-    component: ServersComponent
+    component: ServersComponent,
+    children: [
+      {
+        path: ':id',
+        component: ServerComponent
+      },
+      {
+        path: ':id/edit',
+        component: EditServerComponent
+      },
+    ]
   },
 ]
 
