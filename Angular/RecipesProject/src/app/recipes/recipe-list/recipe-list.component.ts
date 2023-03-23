49496b95ada41c0e339adc297b0125c12c5c2ca5
Leandro Luna <leandro.j.luna@gmail.com>
2023-03-23 13:21:29 -0300

    feat(recipes and shopping list components): forms reactive and bug fixing

--- Angular/RecipesProject/src/app/recipes/recipe-list/recipe-list.component.ts
+++ Angular/RecipesProject/src/app/recipes/recipe-list/recipe-list.component.ts
@@ -1,19 +1,28 @@
 import { RecipeService } from './../recipe.service';
-import { EventEmitter, OnInit } from '@angular/core';
+import { EventEmitter, OnDestroy, OnInit } from '@angular/core';
 import { Recipe } from './../recipe.model';
 import { Component, Output } from '@angular/core';
+import { Subscription } from 'rxjs';
 
 @Component({
   selector: 'app-recipe-list',
   templateUrl: './recipe-list.component.html',
   styleUrls: ['./recipe-list.component.css']
 })
-export class RecipeListComponent implements OnInit {
+export class RecipeListComponent implements OnInit, OnDestroy {
   recipes: Recipe[];
+  subscription: Subscription;
 
   constructor(private recipeService: RecipeService){}
 
   ngOnInit(){
+    this.subscription =  this.recipeService.recipesChanged.subscribe((recipe: Recipe[]) => {
+      this.recipes = recipe;
+    })
     this.recipes = this.recipeService.getRecipeslist();
+  }
+
+  ngOnDestroy(): void {
+    this.subscription.unsubscribe();
   }
 }
--- Angular/RecipesProject/src/app/recipes/recipe.service.ts
+++ Angular/RecipesProject/src/app/recipes/recipe.service.ts
@@ -2,9 +2,12 @@ import { ShoppingListService } from './../shopping-list/shopping-list.service';
 import { Injectable,  } from '@angular/core';
 import { Recipe } from './recipe.model';
 import { Ingredient } from '../shared/ingredient.model';
+import { Subject } from 'rxjs';
 
 @Injectable()
 export class RecipeService {
+  recipesChanged = new Subject<Recipe[]>();
+
   private recipes: Recipe[] = [
     new Recipe('A test recipe', 'This is a simply test', 'https://www.foodandwine.com/thmb/gE_M3yiTJCgZPKSOEVmjQowKv9E=/750x0/filters:no_upscale():max_bytes(150000):strip_icc()/Tamarind-Chicken-FT-RECIPE0522-80072d93f7bc4bc7abf1dcf5b5317b0c.jpg', [new Ingredient('Potato', 5), new Ingredient('Chicken', 1)]),
     new Recipe('A test recipe - 2', 'This is a simply test - 2', 'https://www.foodandwine.com/thmb/gE_M3yiTJCgZPKSOEVmjQowKv9E=/750x0/filters:no_upscale():max_bytes(150000):strip_icc()/Tamarind-Chicken-FT-RECIPE0522-80072d93f7bc4bc7abf1dcf5b5317b0c.jpg', [new Ingredient('Fish', 2), new Ingredient('Orange', 3)])
@@ -22,5 +25,21 @@ export class RecipeService {
 
   addIngredientsToShoppingList(ingredients: Ingredient[]){
     this.shoppingListService.addIngredients(ingredients);
+  }
+
+  addRecipe(recipe: Recipe){
+    this.recipes.push(recipe)
+    console.log(this.recipes)
+    this.recipesChanged.next(this.recipes.slice());
+  }
+
+  updateRecipe(index: number, newRecipe: Recipe){
+    this.recipes[index] = newRecipe
+    this.recipesChanged.next(this.recipes.slice());
+  }
+
+  deleteRecipe(index: number){
+    this.recipes.splice(index, 1);
+    this.recipesChanged.next(this.recipes.slice());
   }
 }
